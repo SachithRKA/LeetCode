75d1c41531be742c4543233502b476fc8c3b9058
SachithRKA <sachithRKA@gmail.com>
2024-07-13 16:23:54 -0400

    update

--- maxOperations.js
+++ maxOperations.js
@@ -4,35 +4,29 @@
  * @return {number}
  */
 var maxOperations = function(nums, k) {
-    nums.sort();
+    nums.sort((a, b) => a - b); 
     let pointerOne = 0;
-    let pointerTwo = nums.length -1;
-    let value = pointerOne + pointerTwo;
+    let pointerTwo = nums.length - 1;
     let counter = 0;
 
-    while (sort.length > 1) {
-        pointerOne = 0;
-        pointerTwo = nums.length -1;
-        value = pointerOne + pointerTwo;
-        counter = 0;
+    while (pointerOne < pointerTwo) {
+        let value = nums[pointerOne] + nums[pointerTwo];
 
-        while (pointerOne < pointerTwo) {
-            if (value == k) {
-                counter +=1;
-                nums.splice(pointerOne, 1);
-                nums.splice(pointerTwo, 1);
-            }
-            else if (value > k) {
-
-            }
-            else if (value < k) {
-                
-            }
+        if (value > k) {
+            pointerTwo--;
+        } else if (value < k) {
+            pointerOne++;
+        } else { 
+            counter++;
+            nums.splice(pointerTwo, 1);
+            nums.splice(pointerOne, 1);
+            pointerTwo -= 2; 
         }
     }
-    return nums;
+
+    return counter;
 };
 
-console.log(maxOperations([3,2,3,4,2], 2));
-console.log(maxOperations([3,2,3,4,2], 3));
+console.log(maxOperations([3,2,3,4,2], 4));
+console.log(maxOperations([3,2,3,4,2], 6));
 console.log(maxOperations([1,2,3,4,5], 5));
